d827348c6c675c34eeb5a1959abdd02226b5ad3e
Zicklag <zicklag@katharostech.com>
2025-02-22 10:27:15 -0600

    feat: add links to maintenance page.

--- src/routes/[...path]/+page.svelte
+++ src/routes/[...path]/+page.svelte
@@ -10,5 +10,20 @@
   <div class="text-xl flex flex-col gap-2 items-center">
     <p>Roomy is currently down for maintenance.</p>
     <p>We'll re-open with improvements soon!</p>
+    <p class="max-w-xl text-center mt-4 links">
+      In the meantime feel free to reach out to us on Bluesky <a
+        href="https://bsky.app/profile/roomy.chat">@roomy.chat</a
+      >
+      or on <a href="https://discord.gg/mbQYgFVBQx">Discord</a> or
+      <a href="https://matrix.to/#/#muni-town:commune.sh">Matrix</a>.
+    </p>
   </div>
 </div>
+
+<style>
+  .links a {
+    color: lightskyblue;
+    text-decoration: underline;
+    text-underline-offset: 6px;
+  }
+</style>
